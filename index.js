502bba50bc8654dc18b3fe678f8b088f95722086
walidbelharazem <walidbelharazem@gmail.com>
2023-09-08 23:13:24 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(title => {
+    return title
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  });
+};
+
+module.exports = {titleCased};
